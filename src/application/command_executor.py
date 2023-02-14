6015ab5d3b1d18af0ab0f9ed9f3586987149653c
gprossignoli <gprauxiliar@gmail.com>
2023-02-14 19:14:20 +0100

    Command executor

--- src/application/command_executor.py
+++ src/application/command_executor.py
@@ -5,9 +5,8 @@ from src.application.purchase_book.purchase_book_command_handler import Purchase
 
 class CommandExecutor:
 	__mapper = {
-		PurchaseBookCommand.fqn(): PurchaseBookCommandHandler.execute
+		PurchaseBookCommand.fqn(): PurchaseBookCommandHandler()
 	}
 
 	def execute(self, command: Command) -> None:
-		self.__mapper.get(command.fqn()).__call__(command=command)
-# este call necesita implementacion en la clase creo.
+		self.__mapper.get(command.fqn()).execute(command=command)
