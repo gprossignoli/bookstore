2fe4f410a18641caf1ce1de45f7e707ff70c9d16
gprossignoli <gprauxiliar@gmail.com>
2023-02-18 11:18:58 +0100

    Gen data improved:
    - Books
    - Users
    - All

--- api/admin_routes.py
+++ api/admin_routes.py
@@ -18,14 +18,21 @@ admin_blueprint = Blueprint(name="admin", import_name=__name__, url_prefix="/adm
 
 @admin_blueprint.route("/gen_data", methods=["POST"])
 def gen_data():
-	file_path = f"{ROOT_DIR}/books.csv"
+	__generate_books_data()
+	__generate_users_data()
 
+	return Response(response={"Books and Users inserted in DB"}, status=200)
+
+
+def __generate_books_data(session: Session = None) -> None:
+	file_path = f"{ROOT_DIR}/books.csv"
 	with open(file_path, 'r') as file:
 		reader = csv.DictReader(file)
 		rows = [row for row in reader]
 		rows = rows[:10000]
 
-	session = Session(db)
+	if session is None:
+		session = Session(db)
 
 	for row in rows:
 		authors = row['authors']
@@ -38,7 +45,8 @@ def gen_data():
 			continue
 
 		publisher = row['publisher']
-		book = Book(title=title, authors=authors, isbn=isbn, publication_date=publication_date, publisher=publisher)
+		book = Book(title=title, authors=authors, isbn=isbn, publication_date=publication_date, publisher=publisher,
+					stock=100000000)
 		print(book.title, book.isbn)
 		try:
 			session.add(book)
@@ -47,7 +55,43 @@ def gen_data():
 			session.rollback()
 			print(e)
 
-	return Response(response={"Books inserted in DB"}, status=200)
+
+def __generate_users_data(session: Session = None) -> None:
+	users = [
+		User(id="e5e61e0d-9bf9-4e54-9d1e-7d4b4af1e7a3", username="johndoe123", first_name="Emily",
+			 last_name="Rodriguez", email="johnsmith@example.com", user_status=True),
+		User(id="c3f4d9b9-037b-4b04-9e11-f2b37dc371a6", username="sarahsmith", first_name="Jacob", last_name="Kim",
+			 email="jane.doe@gmail.com", user_status=True),
+		User(id="a593c421-7147-4b88-a32f-af200109a1bc", username="brian.williams", first_name="Sophia",
+			 last_name="Patel", email="mario.rossi@hotmail.com", user_status=True),
+		User(id="4f80b2cf-cb4c-4a4a-b9c9-7e24b918c224", username="katie88", first_name="William", last_name="Chen",
+			 email="amy.nguyen@yahoo.com", user_status=True),
+		User(id="28d082e1-7ca7-4d72-b7e7-9c9b7f8256b9", username="markjones27", first_name="Natalie", last_name="Wong",
+			 email="tom.baker@outlook.com", user_status=True),
+	]
+
+	if session is None:
+		session = Session(db)
+
+	for user in users:
+		try:
+			session.add(user)
+			session.commit()
+		except Exception as e:
+			session.rollback()
+			print(e)
+
+
+@admin_blueprint.route("/gen_users_data", methods=["POST"])
+def gen_users_data():
+	__generate_users_data()
+	return Response(response={"Users inserted in DB"}, status=200)
+
+
+@admin_blueprint.route("/gen_books_data", methods=["POST"])
+def gen_books_data():
+	__generate_books_data()
+	return Response(response={"Books and Users inserted in DB"}, status=200)
 
 
 @admin_blueprint.route("/create_user", methods=["POST"])
@@ -55,7 +99,8 @@ def create_user():
 	data = __get_data_from_request()
 	body = __validate_create_user_body(data)
 
-	user = User(username=body["username"], first_name=body["first_name"], last_name=body["last_name"], email=body["email"])
+	user = User(username=body["username"], first_name=body["first_name"], last_name=body["last_name"],
+				email=body["email"])
 	session = Session(db)
 	try:
 		session.add(user)
@@ -107,7 +152,8 @@ def __validate_create_user_body(data: Dict) -> Union[Dict, Response]:
 		val = v.validate(body, schema)
 
 		if not val:
-			return Response(response=f"Invalid request: {ujson.dumps(v.errors)}", status=400, mimetype='application/json')
+			return Response(response=f"Invalid request: {ujson.dumps(v.errors)}", status=400,
+							mimetype='application/json')
 
 		return v.normalized(body)
 
