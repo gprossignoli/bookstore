66a7bf308c163ab8fa0a767c412bd704ce00b10a
gprossignoli <gprauxiliar@gmail.com>
2023-03-22 17:44:48 +0100

    Remove comment

--- src/bookstore/application/purchase_book/purchase_book_command.py
+++ src/bookstore/application/purchase_book/purchase_book_command.py
@@ -3,12 +3,6 @@ from dataclasses import dataclass
 from bookstore.application.command import Command
 
 
-"""
-Para mañana, esta clase pasa a ser dataclass, se crea un command_executer o algo asi que tenga mappeo interno de los comandos,
-el command handler recibe el comando e implementar commandhandler.
-"""
-
-
 @dataclass()
 class PurchaseBookCommand(Command):
 	book_id: int
